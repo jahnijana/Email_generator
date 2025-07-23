87e75b5c6138b1954a1fa888832fa212eeeab447
JAHNI <jahnijana@gmail.com>
2025-07-23 14:51:17 +0530

    Update email.py

--- email.py
+++ email.py
@@ -23,16 +23,12 @@ Email_pool = [
 
 first_name = input("Enter your first name: ").lower()
 last_name = input("Enter your last name: ").lower()
-
-
 email_domain = input("Enter your email domain (e.g. @gmail.com): ").lower()
-
 
 if not email_domain.startswith("@") or "." not in email_domain or email_domain.count("@")!=1 or ".." in email_domain :
     print("Invalid Email.Enter valid email like 'jahnijana@gmail.com'.")
 else:
     Email= f"{first_name}{last_name}{email_domain}"
-
     if Email in Email_pool:
         print(f"The email {Email} already existed.")
     else:
